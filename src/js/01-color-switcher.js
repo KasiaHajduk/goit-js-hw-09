84f0a78e702746c725411617d0ce0fa765b86004
KasiaHajduk <kasiahajduk2014@gmail.com>
2021-12-04 20:56:06 +0100

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const body = document.querySelector("body");
+const btnStart = document.querySelector("[data-start]");
+const btnStop = document.querySelector("[data-stop]");
+
+let timerId = null;
+
+btnStart.addEventListener("click", () => {
+    btnStart.disabled = true;
+    timerId = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+});
+
+btnStop.addEventListener("click", () => {
+    clearInterval(timerId);
+    btnStart.disabled = false;
+});
