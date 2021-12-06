ee5e7ff50e9524b30d4ffbd601221ff6def0ebba
KasiaHajduk <kasiahajduk2014@gmail.com>
2021-12-06 22:03:52 +0100

    0612-2203

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -14,7 +14,6 @@ btnStart.disabled = true;
 let timerId = null;
 
 let countDate = 0;
-let nowDate = 0;
 
 const options = {
   enableTime: true,
@@ -22,16 +21,15 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
     onClose(selectedDates) {
+        countDays.textContent = countHours.textContent = countMinutes.textContent = countSeconds.textContent = "00";
+        clearInterval(timerId);
         countDate = selectedDates[0];
-        nowDate = calendar.now;
-             
-        if (countDate < nowDate) {
+        if (countDate < calendar.now) {
             Notiflix.Notify.failure('Please choose a date in the future');
             btnStart.disabled = true;
+            return;
         }
-        else {
-            btnStart.disabled = false;
-        } 
+        btnStart.disabled = false;
     },
 };
 
@@ -61,13 +59,54 @@ function addLeadingZero(value) {
 }
 
 
-btnStart.addEventListener("click", () => {
+function timeTimer(){
+    btnStart.disabled = true;
     timerId = setInterval(() => {
-        let count = convertMs(countDate.getTime() - new Date);
+        const actualTime = new Date();
+        const time = (countDate.getTime() - actualTime.getTime());
+        console.log(time);
+        if (time <= 0) {
+            clearInterval(timerId);
+            btnStart.disabled = false;
+            return;
+        }
+        const count = convertMs(time);
         countDays.textContent = addLeadingZero(count.days);
         countHours.textContent = addLeadingZero(count.hours);
         countMinutes.textContent = addLeadingZero(count.minutes);
         countSeconds.textContent = addLeadingZero(count.seconds);
     }, 1000);
-});
+    
 
+};
+
+btnStart.addEventListener("click", timeTimer);
+
+
+//INTERFEJS
+const input = document.querySelector('input');
+input.style.fontSize = '35px';
+input.style.marginLeft = '30px';
+
+btnStart.style.fontSize = '30px';
+btnStart.style.padding = '4px';
+
+const timer = document.querySelector('.timer');
+timer.style.marginTop = '30px';
+timer.style.display = 'flex';
+timer.style.flexDirection = 'flex';
+
+const timerClocks = document.querySelectorAll('.field');
+for (const timerClock of timerClocks) {
+  timerClock.style.display = 'flex';
+  timerClock.style.flexDirection = 'column';
+  timerClock.style.alignItems = 'center';
+  timerClock.style.marginLeft = '30px';
+  timerClock.style.textTransform = 'uppercase';
+  timerClock.style.fontSize = '25px';
+};
+
+const timerValues = document.querySelectorAll('.value');
+for (const timerValue of timerValues) {
+    timerValue.style.fontSize = '60px';
+};
